19066a51873063904c721eea8768867acd1d6c07
User@DESKTOP-HFA7F9M.(none) <Orlando.Malo@fti.edu.al>
2021-12-21 23:19:05 +0100

    added class Admin

---
+++ PayrollSoftwareConsoleApp/Admin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollSoftwareConsoleApp
+{
+    internal class Admin: Staff
+    {
+
+        private const float overtimeRate = 15.5f;
+        private const float adminHourlyRate = 30;
+
+        public float Overtime
+        {
+            get
+            {
+                return Overtime;
+            }
+            private set
+            {
+                Overtime = value;
+            }
+        }
+
+        public Admin(string name): base(name, adminHourlyRate)
+        {
+
+        }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+            Overtime = overtimeRate * (hoursWorked - 160);
+            if (hoursWorked > 160)
+            {
+                TotalPay = BasicPay + Overtime;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Overtime: "+Overtime+"admin hourly rate:"+adminHourlyRate+"overtime Rate="+overtimeRate;
+        }
+    }
+}
