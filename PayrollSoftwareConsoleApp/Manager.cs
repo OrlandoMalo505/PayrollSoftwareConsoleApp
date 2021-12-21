93e3d3bd5f4bf21edbe224dd926b2af2caf9ec75
User@DESKTOP-HFA7F9M.(none) <Orlando.Malo@fti.edu.al>
2021-12-21 23:08:49 +0100

    added class Manager

---
+++ PayrollSoftwareConsoleApp/Manager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollSoftwareConsoleApp
+{
+    internal class Manager:Staff
+    {
+        private const float managerHourlyRate = 50;
+
+        public int Allowance
+        {
+            get {
+                return Allowance; 
+                }
+            private set
+            {
+                Allowance = value;
+            }
+        }
+
+        public Manager(string name ) : base( name,  managerHourlyRate)
+        {
+
+        }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+            Allowance = 1000;
+            if (hoursWorked > 160)
+            {
+                TotalPay = BasicPay + Allowance;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Allowance= "+Allowance+", Manager Hourly Rate="+managerHourlyRate;
+        }
+    }
+}
