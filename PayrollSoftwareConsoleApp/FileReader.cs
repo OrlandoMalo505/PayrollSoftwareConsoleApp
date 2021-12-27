dc7a03ef3c13be0b211bbdb38c64daf98f6d2ec5
User@DESKTOP-HFA7F9M.(none) <Orlando.Malo@fti.edu.al>
2021-12-27 15:25:51 +0100

    added filereader class

---
+++ PayrollSoftwareConsoleApp/FileReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollSoftwareConsoleApp
+{
+    internal class FileReader
+    {
+
+        public List<Staff> ReadFile()
+        {
+            List<Staff> myStaff=new List<Staff>();
+            string[] result=new string[2];
+            string path = "staff.txt";
+            string[] separator = { ", " };
+
+            if (File.Exists(path))
+            {
+                using(StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        sr.ReadLine();
+                        result = sr.ReadLine().Split(separator,StringSplitOptions.RemoveEmptyEntries);
+                        if (result[1] == "Manager")
+                        {
+                            myStaff.Add(new Manager(result[0]));
+                        }
+                        else if(result[1] == "Admin")
+                        {
+                            myStaff.Add(new Admin(result[0]));
+                        }
+                    }
+                    sr.Close();
+                }
+            }
+            else
+            {
+                Console.WriteLine("File Not Found.");
+            }
+            return myStaff; 
+        }
+    }
+
+}
